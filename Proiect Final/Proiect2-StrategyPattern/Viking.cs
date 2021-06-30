bd7d953988410621e2711ec7ddc85ca66ee7b4a0
cosmin0502 <66167596+cosmin0502@users.noreply.github.com>
2021-06-30 18:15:04 +0300

    Add files via upload

---
+++ Proiect Final/Proiect2-StrategyPattern/Viking.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proiect2_StrategyPattern
+{
+    public class Viking : IUnit
+    {
+        public IMoveBehavior MoveBehavior { get; set; }
+        public int Position { get; set; }
+
+        public Viking()
+        {
+            this.MoveBehavior = new Walk();
+        }
+
+        public void Move()
+        {
+            MoveBehavior.Move(this);
+        }
+    }
+
+    public interface IUnit
+    {
+        int Position { get; set; }
+        void Move();
+        IMoveBehavior MoveBehavior { get; set; }
+    }
+
+    public interface IMoveBehavior
+    {
+        void Move(IUnit unit);
+    }
+
+    public class Fly : IMoveBehavior
+    {
+        public void Move(IUnit unit)
+        {
+            unit.Position += 10;
+        }
+    }
+
+    public class Walk : IMoveBehavior
+    {
+        public void Move(IUnit unit)
+        {
+            unit.Position += 1;
+        }
+    }
+    
+}
---
+++ Proiect Final/Proiect2-StrategyPatternTests/VikingTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Proiect2_StrategyPattern;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proiect2_StrategyPattern.Tests
+{
+    [TestClass()]
+    public class VikingTests
+    {
+        [TestMethod()]
+        public void _0_WalkMove()
+        {
+            IUnit viking = new Viking();
+
+            viking.Move();
+            Assert.AreEqual(1, viking.Position);
+            viking.Move();
+            Assert.AreEqual(2, viking.Position);            
+        }
+
+        public void FlyMove()
+        {
+            IUnit viking = new Viking();
+            viking.MoveBehavior = new Fly();
+
+            viking.Move();
+            Assert.AreEqual(10, viking.Position);
+            viking.Move();
+            Assert.AreEqual(20, viking.Position);
+        }
+
+        public void _2_MixMove()
+        {
+            IUnit viking = new Viking();
+
+            viking.Move();
+            Assert.AreEqual(1, viking.Position);
+
+            viking.MoveBehavior = new Fly();
+            viking.Move();
+            Assert.AreEqual(11, viking.Position);
+        }
+    }
+}
