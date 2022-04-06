34f5fc3f1b32e43b196129365aee3437dbc3a7c5
cosmin0502 <66167596+cosmin0502@users.noreply.github.com>
2022-04-06 17:48:30 +0300

    Add files via upload

---
+++ .github/workflows/DecoratorMementoDesignPatternTests/CalculatePriceHPTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DecoratorMementoDesignPattern;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorMementoDesignPattern.Tests
+{
+    [TestClass()]
+    public class CalculatePriceHPTest
+    {
+        [TestMethod()]
+        public void AddHPLaptopTest()
+        {
+            
+        }
+    }
+
+    [TestClass()]
+    public class CaculateTests
+    {
+        [TestMethod]
+        public void Adding_1000_and_1500_returns2500()
+        {
+            var calculate = new CalculatePriceHP();
+            int result = calculate.AddHPLaptop(1000, 1500);
+            Assert.AreEqual<int>(2500, result);
+        }
+    }
+}
---
+++ .github/workflows/DecoratorMementoDesignPatternTests/NumberLaptopModelsTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DecoratorMementoDesignPattern;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorMementoDesignPattern.Tests
+{
+    [TestClass()]
+    public class NumberLaptopModelsTest
+    {
+        [TestMethod()]
+        public void AddNumberLaptopModelsTest()
+        {
+            
+        }
+
+    }
+
+    [TestClass()]
+    public class NumberModelsTestes
+    {
+        [TestMethod]
+        public void Adding_550_and_950_returns1500()
+        {
+            var numbermodels = new NumberLaptopModels();
+            int resultnumbermodels = numbermodels.AddNumberLaptopModels(550, 950);
+            Assert.AreEqual<int>(1500, resultnumbermodels);
+        }
+    }
+}
